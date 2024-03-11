3d53f94cfad13fe2a40cdd9581188557ef7f1a49
wonkybream <sampo.l@protonmail.com>
2024-03-11 10:25:44 +0200

    Bump version to 2.1.0

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ here = pathlib.Path(__file__).parent.resolve()
 
 setup(
     name="django-rdtwt",
-    version="2.0.0",
+    version="2.1.0",
     license="MIT",
     author="Sampo Lavinen",
     description="Run Django tests with testcontainers.",
